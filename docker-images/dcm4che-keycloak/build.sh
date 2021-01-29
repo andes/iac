e0970cc57368a70270507a514cfe0fc3a236cfaf
Orlando Brea <orlando.brea@gmail.com>
2021-01-29 14:43:33 -0300

    fix(keycloak): correccion deployment (#8)
    * fix(keycloak): correccion deployment
    
    Se corrije deployment de keycloak. Creacion de usuario de andes y creacion de usuario regular
    
    * build(dcm4che): actualizacion a 5.23.0

--- docker-images/dcm4che-keycloak/build.sh
+++ docker-images/dcm4che-keycloak/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t andesnqn/dcm4che-keycloak:11.0.0 .
-docker push andesnqn/dcm4che-keycloak:11.0.0
+docker build -t andesnqn/dcm4che-keycloak:11.0.3 .
+docker push andesnqn/dcm4che-keycloak:11.0.3
--- docker-images/dcm4che-logstash/build.sh
+++ docker-images/dcm4che-logstash/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t andesnqn/dcm4che-logstash:7.8.0-10 .
-docker push andesnqn/dcm4che-logstash:7.8.0-10
+docker build -t andesnqn/dcm4che-logstash:7.9.1-11 .
+docker push andesnqn/dcm4che-logstash:7.8.9-11
