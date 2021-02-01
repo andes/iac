80bdb67410048a72f5e866b62f5380d814ec0aec
Orlando Brea <orlando.brea@gmail.com>
2021-02-01 09:02:50 -0300

    fix(dcm4che): docker actualizada version

--- docker-images/dcm4che-arc-psql/build.sh
+++ docker-images/dcm4che-arc-psql/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --no-cache  -t andesnqn/dcm4chee-arc-psql:5.22.4-secure .
-docker push andesnqn/dcm4chee-arc-psql:5.22.4-secure
+docker build --no-cache  -t andesnqn/dcm4chee-arc-psql:5.23.0-secure .
+docker push andesnqn/dcm4chee-arc-psql:5.23.0-secure
