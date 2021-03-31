2d480c2923bb013ccd089019db23da6874c00e05
Orlando Brea <orlando.brea@gmail.com>
2021-03-31 16:49:50 -0300

    wip: list all PV on all kubernetes configured clusters

--- utilities/check-unused-disks/app.js
+++ utilities/check-unused-disks/app.js
@@ -23,30 +23,84 @@ function parseDiskContent(content) {
     });
 }
 
-async function generateDiskIdsFromKubernetes() {
-  const content = await (() =>
-    new Promise((resolve, reject) => {
-      exec(`kubectl get pv`, (error, stdout, stderr) => {
-        if (error) reject(error);
-        if (stderr) reject(stderr);
-        resolve(stdout);
-      });
-    }))();
-  return content
-    .replaceAll('\t', ' ')
-    .split('\n')
-        .filter((line, idx) => line != '' && idx>0)
-    .map((line) => {
-      const parsedLine = line.match(/(\S+)\s+/);
-      if (parsedLine && parsedLine.length > 1) {
-        return {
-          pvName: parsedLine[1],
-        };
-      }
+async function executeCommand(cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) reject(error);
+      if (stderr) reject(stderr);
+      resolve(stdout);
     });
+  });
 }
 
+async function generateDiskIdsFromKubernetes() {
+  try {
+    const content = await executeCommand(`kubectl get pv`);
+    return content
+      .replaceAll('\t', ' ')
+      .split('\n')
+      .filter((line, idx) => line != '' && idx > 0)
+      .map((line) => {
+        const parsedLine = line.match(/(\S+)\s+/);
+        if (parsedLine && parsedLine.length > 1) {
+          return {
+            pvName: parsedLine[1],
+          };
+        }
+      });
+  } catch (e) {
+    // console.log(e);
+    return [];
+  }
+}
+
+function changeToNextContext() {
+  let currentContextIndex = 0;
+  let contextList = [];
+
+  // const getCurrentKubernetesContext = async () => {
+  //   return executeCommand(`kubectl config current-context`);
+  // };
+
+  const setCurrentKubernetesContext = async (contextName) => {
+    return executeCommand(`kubectl config use-context ${contextName}`);
+  };
+
+  const getContextList = async () => {
+    const contextResponse = await executeCommand(`kubectl config get-contexts`);
+    return contextResponse
+      .replaceAll('\t', ' ')
+      .split('\n')
+      .filter((line, idx) => line != '' && idx > 0)
+      .map((line) => {
+        const parsedLine = line.match(/[\ \*]+(\S+)\s+/);
+        return parsedLine && parsedLine.length > 1 ? parsedLine[1] : undefined;
+      });
+  };
+
+  return async () => {
+    if (contextList.length === 0) {
+      contextList = await getContextList();
+    } else {
+      currentContextIndex++;
+    }
+    if (currentContextIndex > contextList.length - 1) {
+      return false;
+    }
+    await setCurrentKubernetesContext(contextList[currentContextIndex]);
+    return true;
+  };
+}
+
+(async () => {
+  const fileDisks = parseDiskContent(loadVmWareDisks('disks.txt'));
+  // console.log(kubernetesPVCs, fileDisks);
+  let pvcList = [];
+  const changeContext = changeToNextContext();
+  while (await changeContext()) {
+    const list = await generateDiskIdsFromKubernetes();
+    if (list && list.length > 0) pvcList = [...pvcList, ...list];
+  }
+  console.log('Todos los PV son', pvcList);
+})();
 // const diskList = parseDiskContent(loadVmWareDisks('disks.txt'));
-generateDiskIdsFromKubernetes().then(pvc => {
-    console.log(pvc)
-});
