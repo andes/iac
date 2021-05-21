ed584ecbb28ead03e5bc2a50ad30b3becf451759
Orlando Brea <orlando.brea@gmail.com>
2021-05-21 12:14:41 -0300

    fix: privileges script missing ;

--- docker-images/dcm4che-postgres/99_user_privileges.sql
+++ docker-images/dcm4che-postgres/99_user_privileges.sql
@@ -1,3 +1,3 @@
-grant all privileges on all tables in schema public to "PG_USER"
+grant all privileges on all tables in schema public to "PG_USER";
 
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public to "PG_USER"
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public to "PG_USER";
